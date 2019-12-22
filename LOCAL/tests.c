5cb9760b563cb39cb14f04a0d9c11244166b92da
daniel <danielpeer@mail.tau.ac.il>
2019-12-22 15:58:18 +0200

    modified:   tests.c 	modified:   ../FTL/PAGE_MAP/ftl.c 	modified:   ../FTL/PAGE_MAP/ftl_mapping_manager.h

--- LOCAL/tests.c
+++ LOCAL/tests.c
@@ -1,7 +1,5 @@
 #include "ssd.h"
 #include "common.h"
-#include <math.h>
-#include <time.h>
 #include <assert.h>
 
 #define RUN_TEST(SETUP, FUNC)   if(SETUP) popen("rm data/*.dat", "r"); \
@@ -12,49 +10,31 @@
 extern int32_t* mapping_table;
 
 /**
-* get a random number from a range lower to upper, that divides by 
-*/
-int get_random_lba(int lower, int upper) 
-{  
-	int rand_lba = ((rand() % (upper - lower + 1)) + lower); 
-    int rand_lba_modulu_page_sectors = rand_lba % SECTORS_PER_PAGE;
-	if (rand_lba_modulu_page_sectors != 0){
-		rand_lba = rand_lba - rand_lba_modulu_page_sectors;
-	}
-	return rand_lba; 
-} 
-
-/**
- * simple test that writes up to 70% of sectors in the device sequentially or randomly
+ * simple test that writes all sectors in the device sequentially
  */
 int test_access()
 {
-	int i, lba;
-	// SECTORS_PER_PAGE is 8 by deafult and each sector is 512 bytes by default 
-	// which means we need 8 sectors for each 4KB write:
-	int num_sectors_per_write = SECTORS_PER_PAGE; 
-	// we are requested to only write to at most the first 70% of disk sector:
-	long int max_number_of_sectors_to_write = 0.7 * SECTOR_NB; 
+	int ret, i, lba;
 
-	// write up to 70% of entire device 
-	for(i=0; i + num_sectors_per_write < max_number_of_sectors_to_write; i+=num_sectors_per_write){
+	// write entire device 
+	for(i=0;i<SECTOR_NB;i+=SECTORS_PER_PAGE){
+		if ((i/SECTORS_PER_PAGE) % 1024*10==0){
+			LOG("wrote %.3lf of device", (double)i  / (double)SECTOR_NB);
+		}
 
-			lba = i % SECTOR_NB;
-		
-		SSD_WRITE(num_sectors_per_write, lba);
+		lba = i % SECTOR_NB;
+		SSD_WRITE(SECTORS_PER_PAGE, lba);
 	}
 
-	printf("Finished writing workload. \n");
+	printf("wrote seq\n");
 
 	return 0;
 }
 
 int main(int argc, char *argv[]){
-	GARBAGE_COLLECTION();
 	int setup = 1;
-	int req_size_in_4kb_units = atoi(argv[1]);  
-	char workload_type = argv[2][0];
-	RUN_TEST(setup, test_access(req_size_in_4kb_units, workload_type));
+
+	RUN_TEST(setup, test_access());
 
 	return 0;
 }
